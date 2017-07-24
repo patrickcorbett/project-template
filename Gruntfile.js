46a4f3b58989f8b352d902a5be3ed859fdee8509
Patrick Corbett <patrick.corbett@live.com>
2017-07-25 00:05:33 +0200

    Removed Bower

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,23 +1,8 @@
 module.exports = function(grunt) {
 	
-	grunt.loadNpmTasks('grunt-bower-task');
-	
 	grunt.initConfig({
-		
-		// Runs 'bower install'
-        bower: {
-            install: {
-                options: {
-                    targetDir: 'src/main/webapp/vendor',
-                    // https://github.com/yatskevich/grunt-bower-task/issues/134 Task might fail for certain bower packages if copy is not
-                    // explicitly set to false (false should be default but somehow isn't)
-                    copy: false,
-                    prune: true
-                }
-            }
-        }
-		
+			
 	});
 	
-	grunt.registerTask('default',['bower:install']);
+	grunt.registerTask('default');
 };
